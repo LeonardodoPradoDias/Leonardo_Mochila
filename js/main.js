398298009bb6d3609f7e153899986870f0e5242b
LEONARDO <134975430+LeonardodoPradoDias@users.noreply.github.com>
2023-11-20 19:45:41 -0300

    kkkkkkkkkkkk

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ const form = console.log(document.getElementById("novoItem"))
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    console.log(funcionou)
+    
+
+    console.log(evento)
+    console.log(evento.target[0].value)
 })
