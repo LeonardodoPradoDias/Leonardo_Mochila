07a9da986c3b30ef80e995cf625b35b8aab157de
LEONARDO <134975430+LeonardodoPradoDias@users.noreply.github.com>
2023-12-04 19:27:19 -0300

    kkkkkkkkkkkkk

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
-const form = console.log(document.getElementById("novoItem"))
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    
-    criarElemento(nome,quantidade)
-})
 
-function criarElemento(nome,quantidade){
-    console.log(nome)
-    console.log(quantidade)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
-     //<li class="item"><strong>7</strong>Camisas</li>
-   const novoItem = document.criateElemento('li')
-   novoItem.classList.add("item")
-   const numeroItem = document.criateElemento('strong')
-   numeroItem.innerHTML = quantidade
-
-   novoItem.appendChild(numeroItem)
-   novoItem.innerHTML += nome
-
-   const lista = document.getElementById("lista")
-
-   lista.appendChild(novoItem)
-
-   console.log(novoItem)
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
 }
